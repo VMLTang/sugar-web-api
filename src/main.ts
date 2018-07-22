f76ecf8ee0868315624b4df5ba643dc45ba9dcc8
Patrick Housley <patrick.f.housley@gmail.com>
2018-07-22 03:12:20 -0500

    Adding cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { configServer } from './shared/config/server.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true, bodyParser: true });
   configServer(app);
   await app.listen(process.env.PORT || 3000);
 }
