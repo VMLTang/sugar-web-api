e62449be5ddd9ec143ea096fa288996b62b5b453
Patrick Housley <patrick.f.housley@gmail.com>
2018-07-21 22:17:27 -0500

    Adding correct port setting

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { configServer } from './shared/config/server.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   configServer(app);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
